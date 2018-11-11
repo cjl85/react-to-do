484d0a8adff70b959b3feba8b84ed0e8227fbf18
CL <cl@CLs-MacBook-Pro.local>
2018-11-11 11:56:55 -0700

    recent changes events

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ class App extends Component {
         todos: [
           { description: 'Walk the cat', isCompleted: true },
           { description: 'Throw the dishes away', isCompleted: false },
-          { description: 'Buy new dishes', isCompleted: false }
-        ],
+          { description: 'Buy new dishes', isCompleted: false }],
         newTodoDescription: ''
       };
     }
+
+    deleteToDo() {
+      console.log('hi');
+    }
+
+
 
     handleChange(e) {
      this.setState({ newTodoDescription: e.target.value })
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
 class ToDo extends Component {
+
   render() {
+
     return (
-      <li>
-         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+      <div>
+      <ul>
+         <input type= "checkbox" checked= { this.props.isCompleted } onChange= { this.props.toggleComplete } />
          <span>{ this.props.description }</span>
-      </li>
+         <button onClick={ this.props.deleteToDo }> X </button>
+      </ul>
+      <div>
+       <ToDo entries={this.state.todo} delete={this.deleteToDo} />
+
+       </div>
+       </div>
     );
   }
 }
